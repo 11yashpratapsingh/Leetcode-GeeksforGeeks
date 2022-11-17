3d4ce4d8c7721a280fec879ad3df28e07e154c38
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-17 20:48:50 +0530

    Time: 12 ms (69.59%), Space: 5.8 MB (75.43%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,12 +1,17 @@
-class Solution
-{
-    public:
-        int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
-        {
-            int area1 = (ax2 - ax1) *(ay2 - ay1);
-            int area2 = (bx2 - bx1) *(by2 - by1);
-
-            if (ax1 >= bx2 || ax2 <= bx1 || ay2 <= by1 || ay1 >= by2) return area1 + area2;
-            return area1 - (min(bx2, ax2) - max(ax1, bx1)) *(min(ay2, by2) - max(by1, ay1)) + area2;
-        }
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int lt_bound = max(ax1,bx1);
+        int rt_bound = min(ax2,bx2);
+        int tp_bound = min(ay2,by2);
+        int bt_bound = max(ay1,by1);
+        
+        int area1 = (ax2-ax1)*(ay2-ay1);
+        int area2 = (bx2-bx1)*(by2-by1);
+        int inters = 0;
+        
+        if(lt_bound < rt_bound && bt_bound < tp_bound) inters = (rt_bound - lt_bound) * (tp_bound - bt_bound);
+        
+        return area1 + area2 - inters;
+    }
 };
