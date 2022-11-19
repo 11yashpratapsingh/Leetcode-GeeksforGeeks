ea5b555c4e96f781209c83fcb3f2668878122ddb
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-19 18:46:02 +0530

    Time: 146 ms (43.20%), Space: 23.2 MB (33.60%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    int getRotAgle(vector<int>& a, vector<int>& b, vector<int>& c){
+        // (x2-x1)(y3-y1) - (y2-y1)(x3-x1) -> 3D Cross-product of AB and AC vectors
+        return ((b[0] - a[0])*(c[1]-a[1])) - ((b[1]-a[1])*(c[0]-a[0]));
+    }
+    
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        int n = trees.size();
+        if(n<=3) return trees;
+        
+        sort(trees.begin(),trees.end());
+        
+        vector<vector<int>> upHull,lwHull;
+        
+        upHull.push_back(trees[0]);
+        upHull.push_back(trees[1]);
+        
+        for(int i=2;i<n;i++){
+            int upSz = upHull.size();
+            
+            while(upHull.size() >=2 && getRotAgle(upHull[upSz-2],upHull[upSz-1],trees[i])>0){
+                upHull.pop_back();
+                upSz--;
+            }
+            
+            upHull.push_back(trees[i]);
+        }
+        
+        lwHull.push_back(trees[n-1]);
+        lwHull.push_back(trees[n-2]);
+        for(int i=n-3;i>=0;i--){
+            int lwSz = lwHull.size();
+            
+            while(lwHull.size() >=2 && getRotAgle(lwHull[lwSz-2],lwHull[lwSz-1],trees[i])>0){
+                lwHull.pop_back();
+                lwSz--;
+            }
+            
+            lwHull.push_back(trees[i]);
+        }
+        
+        upHull.insert(upHull.end(),lwHull.begin(),lwHull.end());
+        
+        sort(upHull.begin(),upHull.end());
+        upHull.erase(unique(upHull.begin(),upHull.end()),upHull.end());
+        
+        return upHull;
+    }
+};
