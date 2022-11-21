e48f26d54424449364a6a3ab2923a0bab3805dc5
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-21 17:12:43 +0530

    Time: 319 ms (43.97%), Space: 29.7 MB (98.88%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     
-    vector<int> dx = {1 , -1 ,  0 , 0}, dy = {0 ,  0 , -1 , 1};
-    int m,n;
-    
-    bool isValid(int x, int y){
-        return x>=0 && y>=0 && x<m && y<n;
-    }
+    vector<int> dx = {0,1,0,-1}, dy = {1,0,-1,0};
     
     int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
-        m = maze.size(), n = maze[0].size();
-        vector<vector<bool>> vis(m, vector<bool> (n,0));
+        int m = maze.size(), n = maze[0].size();
         queue<pair<int,int>> q;
         
-        vis[ent[0]][ent[1]] = 1;
+        maze[ent[0]][ent[1]] = '+';
         q.push({ent[0],ent[1]});
-        
         int move = 0;
         
         while(!q.empty()){
             int sz = q.size();
-            
             move++;
             while(sz--){
                 auto curr = q.front();
                 q.pop();
-                
                 for(int i=0;i<4;i++){
                     int x = curr.first + dx[i], y = curr.second + dy[i];
-                    if(x>=0 && y>=0 && x<m && y<n){
-                        if(maze[x][y] != '.' || vis[x][y]) continue;
-                    
-                        if(x == 0 || y == 0 || x == m-1 || y == n-1) return move;
-
-                        vis[x][y] = 1;
-                        q.push({x,y});
-                    }
+                    if(x<0 || y<0 || x>=m || y>=n || maze[x][y] == '+') continue;
+                    if(x==0 || y==0 || x==m-1 || y == n-1) return move;
+                    maze[x][y] = '+';
+                    q.push({x,y});
                 }
             }
         }
