f59b10b83e50cf3aa93a57052c538b2d977da70b
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-22 13:49:55 +0530

    Time: 217 ms (74.26%), Space: 9.1 MB (69.87%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -3,10 +3,11 @@ public:
     int numSquares(int n) {
         vector<int> dp(n+1, INT_MAX);
         dp[0] = 0;
-        for(int i=1;i*i<=n;i++){
-            int sq = i*i;
-            for(int j=sq;j<n+1;j++){
-                dp[j] = min(dp[j],dp[j-sq]+1);
+        for(int i=1;i<=n;i++){
+            dp[i] = i;
+            for(int j=1;j*j<=i;j++){
+                int sq = j*j;
+                dp[i] = min(dp[i],dp[i-sq]+1);
             }
         }
         
