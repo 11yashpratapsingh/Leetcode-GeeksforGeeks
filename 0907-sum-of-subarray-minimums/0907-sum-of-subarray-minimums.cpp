8941e96b01e8b36dff7b3988aa6f6a628cca0563
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-25 09:50:49 +0530

    Time: 269 ms (24.26%), Space: 40.2 MB (84.17%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    int mod = 1e9+7;
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        stack<int> st;
+        int n = arr.size();
+        int lt[n];
+        int rt[n];
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            if(st.empty()) lt[i] = i+1;
+            else lt[i]=i-st.top();
+            st.push(i);
+        }
+        
+        while(!st.empty()){
+            st.pop();
+        }
+       
+        //right part
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            if(st.empty()) rt[i] = n-i;
+            else rt[i]=st.top()-i;
+            st.push(i);
+        }
+        
+        int maxA = 0;
+        for (int i = 0; i < n; i++) {
+            long long prod = (lt[i]*rt[i])%mod;
+            prod = (prod*arr[i])%mod;
+            maxA = (maxA + prod)%mod;
+        }
+        return maxA;
+    }
+};
