6cd18afa23bcd687a3e911a4d4b38aa56da285ad
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-29 21:26:02 +0530

    Time: 548 ms (39.56%), Space: 97.1 MB (58.37%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,38 @@
+class RandomizedSet {
+public:
+    vector<int> v;
+    unordered_map<int,int> mp;
+    
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val)!=mp.end()) return false;
+        v.push_back(val);
+        mp[val] = v.size()-1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val) == mp.end()) return false;
+        int last = v.back();
+        mp[last] = mp[val];
+        v[mp[val]] = last;
+        v.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return v[rand()%v.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
