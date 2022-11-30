f096551764d90016d41f379a2ec1b54d25c9812a
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-11-30 14:53:03 +0530

    Time: 8 ms (37.44%), Space: 8 MB (63.54%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        
+        unordered_set<int> st;
+        for(auto it:mp) st.insert(it.second);
+        
+        return mp.size() == st.size();
+    }
+};
