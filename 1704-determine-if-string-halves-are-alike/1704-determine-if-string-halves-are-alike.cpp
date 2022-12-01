3b1f779393156447c6a0e280fde49f708a4a6b15
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-12-01 10:10:40 +0530

    Time: 11 ms (20.12%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
+    string vowels = "aeiouAEIOU";
+    
     bool halvesAreAlike(string s) {
-        int half = s.length()/2;
-        string s1 = s.substr(0,half);
-        string s2 = s.substr(half);
-        
-        int cnt1=0, cnt2 = 0;
-        
-        for(int i=0;i<s1.length();i++){
-            if(s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u' || s1[i] == 'A' || s1[i] == 'E' || s1[i] == 'I' || s1[i] == 'O' || s1[i] == 'U') cnt1++;
+        int mid = s.length()/2, cnt = 0;
+        for(int i=0,j=mid;i<mid;i++,j++){
+            if(~vowels.find(s[i])) cnt++;
+            if(~vowels.find(s[j])) cnt--;
         }
         
-        for(int i=0;i<s2.length();i++){
-            if(s2[i] == 'a' || s2[i] == 'e' || s2[i] == 'i' || s2[i] == 'o' || s2[i] == 'u' || s2[i] == 'A' || s2[i] == 'E' || s2[i] == 'I' || s2[i] == 'O' || s2[i] == 'U') cnt2++;
-        }
-        
-        return cnt1 == cnt2;
+        return cnt == 0;
     }
 };
