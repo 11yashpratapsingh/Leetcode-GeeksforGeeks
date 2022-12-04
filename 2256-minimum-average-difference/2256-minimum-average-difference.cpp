b772961bca1b20f24337f997f02f5cfaf6dd07ed
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-12-04 15:53:42 +0530

    Time: 294 ms (49.15%), Space: 78.3 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
         int n = nums.size();
-        long totSum = 0;
-        for(int i=0;i<n;i++){
-            totSum += nums[i];
-        }
+        long totSum = 0, currSum = 0;
+        for(int i=0;i<n;i++) totSum += nums[i];
         
-        long curSum = 0;
-        int val = INT_MAX;
         int idx = 0;
+        int val = INT_MAX;
+        
         for(int i=0;i<n;i++){
-            curSum += nums[i];
-            int avg1 = curSum/(i+1);
+            currSum += nums[i];
+            int avg1 = currSum/(i+1);
             if(i==n-1){
                 if(avg1<val) return n-1;
                 else break;
             }
             
-            int avg2 = (totSum - curSum)/(n-i-1);
-            if(abs(avg1-avg2)<val){
-                val = abs(avg1-avg2);
+            int avg2 = (totSum - currSum)/(n-i-1);
+            if(abs(avg2-avg1)<val){
+                val = abs(avg2-avg1);
                 idx = i;
             }
         }
