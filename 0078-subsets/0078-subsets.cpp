fdb58c9b2f9316a67aed36378a470f275ee4c0e5
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2023-06-15 11:55:42 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (53.04%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     
     void func(vector<int>& nums, vector<vector<int>>& ans, vector<int> tmp, int idx){
-        if(idx>= nums.size()){
-            ans.push_back(tmp);
-            return;
-        }
+        ans.push_back(tmp);
         
-        tmp.push_back(nums[idx]);
-        func(nums,ans,tmp,idx+1);
-        tmp.pop_back();
-        func(nums,ans,tmp,idx+1);
+        for(int i=idx;i<nums.size();i++){
+            tmp.push_back(nums[i]);
+            func(nums,ans,tmp,i+1);
+            tmp.pop_back();
+        }
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
