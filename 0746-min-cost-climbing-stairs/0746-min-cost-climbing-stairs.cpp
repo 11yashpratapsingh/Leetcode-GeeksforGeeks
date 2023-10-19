b9f9a47efaae6d28b2a798486dfca288a1d2095f
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2023-10-19 23:56:34 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (12.14%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -10,10 +10,23 @@ public:
         return v[n];
     }
     
+    int solve2(vector<int>& cost, int n){
+        vector<int> dp(n+1);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
+        }
+                                  
+        return min(dp[n-1],dp[n-2]);
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> v(n+1,-1);
-        int ans = min(solve(cost,n-1,v),solve(cost,n-2,v));
-        return ans;
+        //vector<int> v(n+1,-1);
+        //int ans = min(solve(cost,n-1,v),solve(cost,n-2,v));
+        return solve2(cost,n);
     }
 };
