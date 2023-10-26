11d36f54a3d0d346f4201ef9d2906c5c34c35fb1
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2023-10-26 19:02:34 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -23,16 +23,32 @@ class Solution
         return dp[n];
     }
     
+    int solveTab(int n, int x, int y, int z){
+        vector<int> dp(n+1,INT_MIN);
+        
+        dp[0] = 0;
+        
+        for(int i=1;i<=n;i++){
+            if(i-x >= 0) dp[i] = max(dp[i],dp[i-x] + 1);
+            if(i-y >= 0) dp[i] = max(dp[i],dp[i-y] + 1);
+            if(i-z >= 0) dp[i] = max(dp[i],dp[i-z] + 1);
+        }
+        
+        return dp[n]<0 ? 0 : dp[n];
+    }
+    
     //Function to find the maximum number of cuts.
     int maximizeTheCuts(int n, int x, int y, int z)
     {
         //Your code here
-        vector<int> v(n+1,-1);
+        //vector<int> v(n+1,-1);
         // int ans = solve(n,x,y,z);
         // return ans<0 ? 0 : ans;
         
-        int ans = solve(n,x,y,z,v);
-        return ans<0 ? 0 : ans;
+        //int ans = solve(n,x,y,z,v);
+        //return ans<0 ? 0 : ans;
+        
+        return solveTab(n,x,y,z);
     }
 };
 
