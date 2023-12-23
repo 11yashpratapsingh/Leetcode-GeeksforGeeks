09b4de9c36792c37613b41f55230272835b85f76
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2023-12-23 20:55:00 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (31.35%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div983-minimum-cost-for-tickets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div983-minimum-cost-for-tickets.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div983-minimum-cost-for-tickets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div983-minimum-cost-for-tickets.cpp
@@ -56,12 +56,30 @@ public:
         }
         
         return dp[0];
-    } 
+    }
+    
+    int solveSpaceOpt(vector<int>& days, vector<int>& costs){
+        int ans = 0;
+        queue<pair<int,int>> month;
+        queue<pair<int,int>> week;
+        for(int day: days){
+            while(!month.empty() && month.front().first + 30 <= day) month.pop();
+            while(!week.empty() && week.front().first + 7 <= day) week.pop();
+            
+            week.push(make_pair(day,ans+costs[1]));
+            month.push(make_pair(day,ans+costs[2]));
+            
+            ans = min(ans+costs[0], min(week.front().second, month.front().second));
+        }
+        
+        return ans;
+    }
     
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         //vector<int> dp(n+1, -1);
         //return solveMem(days,costs,n,0,dp);
-        return solveTab(days,costs,n,0);
+        //return solveTab(days,costs,n,0);
+        return solveSpaceOpt(days,costs);
     }
 };
